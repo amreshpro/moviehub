128e592d03283e22c99848c60b47cd3f417e22ff
Amresh Maurya <amresh.terminal@gmail.com>
2024-01-18 22:15:09 +0530

    add env variables

--- src/constants/index.example.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const TMDB_TOKEN = 'Your API Token';
--- src/utils/useFetch.jsx
+++ src/utils/useFetch.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { BASE_URL, TMDB_TOKEN } from '../constants';
 
+// env variable
+
 const useFetch = (url) => {
     const [data, setdata] = useState(null);
     const [loading, setloading] = useState(true);
@@ -21,7 +23,7 @@ const useFetch = (url) => {
             },
             headers: {
                 accept: 'application/json',
-                Authorization: 'Bearer ' + TMDB_TOKEN,
+                Authorization: 'Bearer ' +TMDB_TOKEN,
             },
         };
         axios.request(options).then((data) => {
