5e0544235c89689f3874fe79f0d3a616a2b533c1
Amresh Maurya <amresh.terminal@gmail.com>
2024-01-25 11:56:34 +0530

    setup the project

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+### Amresh Maurya
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className
+    ="bg-blue-200
+     text-blue-800"
+     >App
+     </div>
+  )
+}
+export default App
