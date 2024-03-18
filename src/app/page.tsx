33e4f6cd000aa98e1dac6984cdab060857de9345
Amresh Maurya <amresh.terminal@gmail.com>
2024-03-18 22:51:03 +0530

    update hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,21 @@ import { useFetch } from "@/utils/useFetch";
 
 
 export default function Hero() {
-  const { data, loading, error, page, setPage, totalPages } =
+  const { data:movieData, loading:loadingMovie} =
     useFetch("/trending/movie/day");
-    if(loading) return <Shimmer/>
+    const { data : TVData, loading:loadingTV} =
+    useFetch("/trending/tv/day");
+    if(loadingMovie || loadingTV) return <Shimmer/>
 
   return <div className="hero">
-    <div className="upcoming">
-      <Carousel data={data} />
+    <h1 className="px-2 mx-2 text-xl mb-5">Movie of the Day</h1>
+    <div className="movie-day">
+      <Carousel data={movieData} />
+    </div>
+    <h1 className="mx-2 px-2 text-xl mb-5">TV Show of the Day</h1>
+
+    <div className="tv-day">
+      <Carousel data={TVData} />
     </div>
   </div>
 }
