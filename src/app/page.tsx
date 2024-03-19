64bf3902f5471c60e1821e984db49cc7c3da90c8
Amresh Maurya <amresh.terminal@gmail.com>
2024-03-19 21:30:32 +0530

    add movie and tv of the week

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,34 @@ export default function Hero() {
     useFetch("/trending/movie/day");
     const { data : TVData, loading:loadingTV} =
     useFetch("/trending/tv/day");
-    if(loadingMovie || loadingTV) return <Shimmer/>
+
+    const { data:movieDataWeek, loading:loadingMovieWeek} =
+    useFetch("/trending/movie/week");
+
+    const { data:TVDataWeek, loading:loadingTVWeek} =
+    useFetch("/trending/tv/week");
+    if(loadingMovie || loadingTV||loadingMovieWeek || loadingTVWeek) return <Shimmer/>
 
   return <div className="hero mt-5">
     <h1 className="px-2 mx-2 text-xl mb-5 mt-2 ">Movie of the Day</h1>
     <div className="movie-day">
       <Carousel data={movieData} />
     </div>
-    <h1 className="mx-2 px-2 text-xl mb-5">TV Show of the Day</h1>
 
+    <h1 className="mx-2 px-2 text-xl mb-5">TV Show of the Day</h1>
     <div className="tv-day">
       <Carousel data={TVData} />
     </div>
+
+    <h1 className="mx-2 px-2 text-xl mb-5">Movie of the Week</h1>
+    <div className="tv-week">
+      <Carousel data={movieDataWeek} />
+    </div> 
+      <h1 className="mx-2 px-2 text-xl mb-5">TV Show of the Week</h1>
+    <div className="tv-week">
+      <Carousel data={TVDataWeek} />
+    </div>
+
+
   </div>
 }
